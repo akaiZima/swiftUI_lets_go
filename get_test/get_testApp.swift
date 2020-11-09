075e20e6d42e35fc14afeb62df7bd63cf833d90e
akai <akai@fawstudio.com>
2020-11-09 19:21:00 +0800

    Initial Commit

---
+++ get_test/get_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  get_testApp.swift
+//  get_test
+//
+//  Created by akaiHuangMBP on 2020/11/9.
+//
+
+import SwiftUI
+
+@main
+struct get_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
